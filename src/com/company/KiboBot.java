468d4fbd93aba7b24a16dc68620c2b5ad3ce2795
Daniel Mezo <daniel.mezo@students.salle.url.edu>
2020-05-30 10:37:24 +0200

    botones and user class

--- src/com/company/KiboBot.java
+++ src/com/company/KiboBot.java
@@ -42,6 +42,29 @@ public class KiboBot extends TelegramLongPollingBot {
             //            .setChatId(chat_id)
             //            .setPhoto("AgADAgAD6qcxGwnPsUgOp7-MvnQ8GecvSw0ABGvTl7ObQNPNX7UEAAEC")
             //            .setCaption("Photo");
+            //PER FER BOTONS
+            //// Create ReplyKeyboardMarkup object
+            //    ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            //    // Create the keyboard (list of keyboard rows)
+            //    List<KeyboardRow> keyboard = new ArrayList<>();
+            //    // Create a keyboard row
+            //    KeyboardRow row = new KeyboardRow();
+            //    // Set each button, you can also use KeyboardButton objects if you need something else than text
+            //    row.add("Row 1 Button 1");
+            //    row.add("Row 1 Button 2");
+            //    row.add("Row 1 Button 3");
+            //    // Add the first row to the keyboard
+            //    keyboard.add(row);
+            //    // Create another keyboard row
+            //    row = new KeyboardRow();
+            //    // Set each button for the second line
+            //    row.add("Row 2 Button 1");
+            //    row.add("Row 2 Button 2");
+            //    row.add("Row 2 Button 3");
+            //    // Add the second row to the keyboard
+            //    keyboard.add(row);
+            //    // Set the keyboard to the markup
+            //    keyboardMarkup.setKeyboard(keyboard);
             if (update.getMessage().getText().equals("/start")) {
                 Intro intro = new Intro(historia.getMissatges().get(0).getText());
                 List<SendMessage> messages = intro.showIntro(chat_id);
---
+++ src/com/company/User.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class User {
+
+    private String name;
+
+    public User()
+    {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
